632ceca5aa1fff92dc5c15af057b9612f0dae151
Harvey Le <harveyxcle@gmail.com>
2021-01-31 17:53:49 -0500

    Added Library and adjust scroll

--- src/App.js
+++ src/App.js
@@ -3,16 +3,25 @@ import './styles/app.scss';
 //Adding Components
 import Player from './components/Player';
 import Song from './components/Song';
+import Library from './components/Library';
+//Import Data
 import data from './data';
 
 function App() {
   //State
   const [songs, setSongs] = useState(data());
-  const [currentSong, setCurrentSong] = useState(songs[3]);
+  const [currentSong, setCurrentSong] = useState(songs[2]);
+  const [isPlaying, setIsPlaying] = useState(false);
+
   return (
     <div className="App">
       <Song currentSong={currentSong} />
-      <Player currentSong={currentSong} />
+      <Player 
+        setIsPlaying={setIsPlaying} 
+        isPlaying={isPlaying} 
+        currentSong={currentSong} 
+      />
+      <Library songs={songs} setCurrentSong={setCurrentSong} />
     </div>
   );
 }
