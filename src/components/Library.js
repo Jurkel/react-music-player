9ef6b527ef085748945908432fd1f01f692f232f
Harvey Le <harveyxcle@gmail.com>
2021-02-01 14:00:13 -0500

    Updated Library

--- src/components/Library.js
+++ src/components/Library.js
@@ -6,11 +6,12 @@ const Library = ({ songs, setCurrentSong }) => {
     <div className='library'>
       <h2>Library</h2>
       <div className='library-songs'>
-        {songs.map(song => (
+        {songs.map((song) => (
           <LibrarySong 
             songs={songs} 
             setCurrentSong={setCurrentSong} 
             song={song.id}  
+            id={song.id}
             key={song.id}
           />
         ))}
--- src/components/LibrarySong.js
+++ src/components/LibrarySong.js
@@ -2,12 +2,12 @@ import React from 'react';
 
 const LibrarySong = ({ song, songs, setCurrentSong, id }) => {
   const songSelectHandler = () => {
-    const selectedSong = song;
+    const selectedSong = songs.filter(state => state.id === id)
     setCurrentSong(selectedSong[0]);
   }
   return (
     <div onClick={songSelectHandler} className='library-song'>
-      <img src={song.cover} alt={song.name}/>
+      <img src={song.cover} alt={song.name} />
       <div className='song-description'>
         <h3>{song.name}</h3>
         <h4>{song.artist}</h4>
